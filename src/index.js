ca599f6e56f4b545ac729f3dd0bc5e83c2b2d9e1
safal-t <safaltimalsena0@gmail.com>
2025-05-14 06:09:07 +1000

    test: update styles and modify index.js for testing purposes
    Made changes to `src/styles.css` to adjust styling for testing.
    Updated `src/index.js` to include modifications for testing functionality.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './styles.css'
 import createTask from './modules/logic/Task.js';
 import TaskList from './modules/logic/Task-List.js';
+import displayList from './modules/dom/display-list.js';
 
 const defaultList = new TaskList
-const firstTask = createTask('First Task', defaultList, 'this is a auto generated task', '00/00/00', 'medium');
+const firstTask = createTask('First Task', defaultList, 'this is an auto-generated task', '00/00/00', 'medium');
+const secondTask = createTask('Second Task', defaultList, 'this is another auto-generated task', '01/01/01', 'high');
+const thirdTask = createTask('Third Task', defaultList, 'yet another auto-generated task', '02/02/02', 'low');
+const fourthTask = createTask('Fourth Task', defaultList, 'final auto-generated task', '03/03/03', 'medium');
+
+displayList(defaultList);
