aeddaac35db1620225c750785448e27feefbb892
safal-t <safaltimalsena0@gmail.com>
2025-05-14 06:08:36 +1000

    feat(dom): add module for displaying lists on the DOM
    Implemented a new module `display-list.js` to handle rendering lists onto the DOM. This improves separation of concerns and modularity in the codebase.

---
+++ src/modules/dom/display-list.js
@@ -0,0 +1,18 @@
+const displayList = list => {
+    const container = document.querySelector(".list-container")
+    list.tasks.forEach(element => {
+        const itemContainer = document.createElement("div");
+        const item = document.createElement("input");
+        item.type = "checkbox";
+        item.id = element.name 
+
+        const label = document.createElement("label")
+        label.htmlFor = element.name;
+        label.textContent = `${element.name}: ${element.description}`;
+
+        itemContainer.append(item, label)
+        container.append(itemContainer)
+    });
+}
+
+export default displayList;
