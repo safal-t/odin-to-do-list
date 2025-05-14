4dfd4ae4227429e1f8c0097d33921cc8cb36a39f
safal-t <safaltimalsena0@gmail.com>
2025-05-15 07:30:54 +1000

    fix: include missed changes in display-options module
    Updated the `display-options.js` module to remove the need for passing arguments when calling the display options function. This change was intended to be part of the previous commit but was accidentally omitted.

--- src/modules/dom/display-options.js
+++ src/modules/dom/display-options.js
@@ -1,5 +1,9 @@
-const displayOptions = (listManager, container) => {
-    container.innerHTML = "";
+import listManager from "../logic/List-Manager";
+
+const CONTAINER = document.querySelector('.list-options-container');
+
+const displayOptions = () => {
+    CONTAINER.innerHTML = "";
     const allLists = listManager.lists; 
     console.log(allLists)
     allLists.forEach(element => {
@@ -7,13 +11,13 @@ const displayOptions = (listManager, container) => {
             const buffer = document.createElement("div");
             buffer.className = "list-options-buffer";
             buffer.textContent = "|";
-            container.append(buffer)
+            CONTAINER.append(buffer)
         }
         const div = document.createElement("div");
         div.className = "list-option-container";
         div.innerText = element.listName;
         div.setAttribute("data-list-name", element.listName);
-        container.append(div); 
+        CONTAINER.append(div); 
     });
 }
 
