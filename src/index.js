a820736ea1b351f612ac2e387a962aa96beba717
safal-t <safaltimalsena0@gmail.com>
2025-05-15 07:18:41 +1000

    style: refine CSS for list display options
    Enhanced the styling of list display options in `styles.css` for better visual consistency and improved user experience.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import createTask from './modules/logic/Task.js';
 import createList from './modules/logic/Task-List.js';
 import displayList from './modules/dom/display-list.js';
 import listManager from './modules/logic/List-Manager.js';
+import displayOptions from './modules/dom/display-options.js';
+
 
 const defaultList = createList("defaultList");
+const testList = createList("testList");
+
 
 createTask('First Task', defaultList, 'this is an auto-generated task', '00/00/00', 'medium');
 createTask('Second Task', defaultList, 'this is another auto-generated task', '01/01/01', 'high');
@@ -12,3 +16,6 @@ createTask('Third Task', defaultList, 'yet another auto-generated task', '02/02/
 createTask('Fourth Task', defaultList, 'final auto-generated task', '03/03/03', 'medium');
 
 displayList(defaultList);
+displayList(testList); 
+
+displayOptions(listManager, document.querySelector('.list-options-container')); 
